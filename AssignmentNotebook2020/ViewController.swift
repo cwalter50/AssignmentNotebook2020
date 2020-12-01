a301be8ea707f49600390224f81fc1d38d8d4613
cwalter50 <walt50@gmail.com>
2020-12-01 09:23:57 -0500

    Day2 Complete

--- AssignmentNotebook2020/ViewController.swift
+++ AssignmentNotebook2020/ViewController.swift
@@ -7,16 +7,19 @@
 
 import UIKit
 
-class ViewController: UIViewController
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
+    
+    
 
     @IBOutlet weak var myTableView: UITableView!
-    
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myTableView.delegate = self
+        myTableView.dataSource = self
+        
     }
 
     @IBAction func addButtonTapped(_ sender: UIBarButtonItem)
@@ -29,6 +32,25 @@ class ViewController: UIViewController
     {
         
         
+    }
+    
+    // MARK: TableView Methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        
+        // change the cells properties...
+        cell.textLabel?.text = "Collin"
+        cell.backgroundColor = UIColor.purple
+        cell.textLabel?.textColor = UIColor.white
+        cell.detailTextLabel?.text = "is the best linebacker I know..."
+        cell.detailTextLabel?.textColor = UIColor.green
+        
+        return cell
     }
 }
 
